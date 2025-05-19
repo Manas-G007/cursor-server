8beb8476c8c0d36ee5abf6bce9ef80e712ca99ac
Manas Gupta <102868339+Manas-G007@users.noreply.github.com>
2025-05-19 23:46:33 +0530

    something:

---
+++ server.js
@@ -0,0 +1,32 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+  
+  socket.on('send-cursor', (data) => {
+    console.log({ id: socket.id, ...data });
+    socket.broadcast.emit('receive-cursor', { id: socket.id, ...data });
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('user-disconnected', socket.id);
+  });
+});
+
+server.listen(6969, () => {
+  console.log("server is running on port 6969");
+});
